dd0254b77dc28829923282d7734574101ef126d0
Mytchel Hammond <mytchel@openmailbox.org>
2015-04-25 13:55:13 +1200

    updated config

--- config.h
+++ config.h
@@ -5,6 +5,6 @@
 #define TAB_WIDTH 4
 
 char bg_name[] = "#000000";
-char fg_name[] = "#aaaaaa";
+char fg_name[] = "#eeeeee";
 
-char font_str[] = "DejaVuSansMono:pixelsize=12:antialias=true:autohint=false";
+char font_str[] = "DejaVu Sans Mono:pixelsize=11:antialias=false";
